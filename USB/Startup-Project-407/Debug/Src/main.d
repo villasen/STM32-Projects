30e07ff0bd5aedbd9f4283ff8360376c1bc2d62b
Martin Villaseñor <43162903+villasen@users.noreply.github.com>
2022-10-21 14:30:43 -0700

    STM32F407 Project init and header file

---
+++ USB/Startup-Project-407/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ /Users/martin/STM32CubeIDE/STM32-Projects/USB/Startup-Project-407/Drivers/Inc/stm32f407xx.h
+/Users/martin/STM32CubeIDE/STM32-Projects/USB/Startup-Project-407/Drivers/Inc/stm32f407xx.h:
