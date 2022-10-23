d1b4bc54eb297c0057072eb82b59e27b3d9b3a80
Martin Villaseñor <43162903+villasen@users.noreply.github.com>
2022-10-23 14:37:41 -0700

    led toggles with button pressed application

--- USB/Startup-Project-407/Src/button_led.c
+++ USB/Startup-Project-407/Src/button_led.c
@@ -1,10 +1,9 @@
 /*
- * ledToggle.c
+ * button_led.c
  *
  *  Created on: Oct 23, 2022
  *      Author: Martin Villasenor
  */
-
 
 
 #include "stm32f407xx.h"
@@ -12,14 +11,14 @@
 void delay(void)
 {
 
-	for (uint32_t i=0; i< 500000 ; i++);
+	for (uint32_t i=0; i< 500000/2 ; i++);
 }
 
 int main(void)
 {
 
 	// create handle
-	GPIO_Handle_t  GpioLed;
+	GPIO_Handle_t  GpioLed, GpioButton;
 
 	GpioLed.pGPIOx = GPIOD;
 	GpioLed.GPIOPinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
@@ -28,20 +27,37 @@ int main(void)
 	GpioLed.GPIOPinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
 	GpioLed.GPIOPinConfig.GPIO_PinSpeed = GPIO_SPEDD_FAST ;
 
+
 	GPIO_PeriClockControl(GPIOD, ENABLE);
 
 	GPIO_Init(&GpioLed);
 
 
+	GpioButton.pGPIOx = GPIOA;
+	GpioButton.GPIOPinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+	GpioButton.GPIOPinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	GpioButton.GPIOPinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	GpioButton.GPIOPinConfig.GPIO_PinSpeed = GPIO_SPEDD_FAST ;
+
+
+	GPIO_PeriClockControl(GPIOA, ENABLE);
+
+	GPIO_Init(&GpioButton);
+
+
+
 	while(1)
 	{
 
-		GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
-		delay();
+		if (GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0))
+		{
+			delay();
+			GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+		}
+
+
 
 	}
 
 	return 0;
 }
-
-
