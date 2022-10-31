f431f0e1f8a7f8b93aecd07b39c659b4eefa55c5
Martin Villaseñor <43162903+villasen@users.noreply.github.com>
2022-10-30 20:22:38 -0700

    Add GPOI and Peripheral macros for STM32F411

--- USB/Startup-Project-411/drivers/Inc/stm32f411xx.h
+++ USB/Startup-Project-411/drivers/Inc/stm32f411xx.h
@@ -161,5 +161,122 @@ typedef struct
 } RCC_RegDef_t;
 
 
+/* GPIO Peripheral accesses addresses */
+
+#define GPIOA    ((GPIO_RegDef_t*)GPIOA_BASEADDR)
+#define GPIOB    ((GPIO_RegDef_t*)GPIOB_BASEADDR)
+#define GPIOC    ((GPIO_RegDef_t*)GPIOC_BASEADDR)
+#define GPIOD    ((GPIO_RegDef_t*)GPIOD_BASEADDR)
+#define GPIOE    ((GPIO_RegDef_t*)GPIOE_BASEADDR)
+#define GPIOF    ((GPIO_RegDef_t*)GPIOF_BASEADDR)
+#define GPIOG    ((GPIO_RegDef_t*)GPIOG_BASEADDR)
+#define GPIOH    ((GPIO_RegDef_t*)GPIOH_BASEADDR)
+#define GPIOI    ((GPIO_RegDef_t*)GPIOI_BASEADDR)
+
+
+/* RCC Peripheral access address */
+
+#define RCC      ((RCC_RegDef_t*)RCC_BASEADDR)
+
+/* EXTI access address */
+#define EXTI     ((EXTI_RedDef_t*)EXTI_BASEADDR)
+
+/* EXTI access address */
+#define SYSCFG   ((SYSCFG_RedDef_t*)SYSCFG_BASEADDR)
+
+/* Clock Enable Macros for GPIOx peripherals */
+
+#define GPIOA_PCLK_EN()  (RCC->AHB1ENR |= (1<<0))
+#define GPIOB_PCLK_EN()  (RCC->AHB1ENR |= (1<<1))
+#define GPIOC_PCLK_EN()  (RCC->AHB1ENR |= (1<<2))
+#define GPIOD_PCLK_EN()  (RCC->AHB1ENR |= (1<<3))
+#define GPIOE_PCLK_EN()  (RCC->AHB1ENR |= (1<<4))
+#define GPIOF_PCLK_EN()  (RCC->AHB1ENR |= (1<<5))
+#define GPIOG_PCLK_EN()  (RCC->AHB1ENR |= (1<<6))
+#define GPIOH_PCLK_EN()  (RCC->AHB1ENR |= (1<<7))
+#define GPIOI_PCLK_EN()  (RCC->AHB1ENR |= (1<<8))
+
+
+/* Clock Enable Macros for I2Cx peripherals */
+#define I2C1_PCLK_EN()   (RCC->APB1ENR |= (1<<21))
+#define I2C2_PCLK_EN()   (RCC->APB1ENR |= (1<<22))
+#define I2C3_PCLK_EN()   (RCC->APB1ENR |= (1<<23))
+
+/* Clock Enable Macros for SPIx peripherals */
+#define SPI1_PCLK_EN()   (RCC->APB2ENR |= (1<<12))
+#define SPI2_PCLK_EN()   (RCC->APB1ENR |= (1<<15))
+#define SPI3_PCLK_EN()   (RCC->APB1ENR |= (1<<14))
+
+/* Clock Enable Macros for USARTx peripherals */
+#define USART1_PCLK_EN()  (RCC->APB2ENR) |= (1<<4))
+#define USART2_PCLK_EN()  (RCC->APB1ENR  |= (1<<17))
+#define USART3_PCLK_EN()  (RCC->APB1ENR  |= (1<<18))
+#define UART4_PCLK_EN()   (RCC->APB1ENR  |= (1<<19))
+#define UART5_PCLK_EN()   (RCC->APB1ENR  |= (1<<20))
+#define USART6_PCLK_EN()  (RCC->APB2ENR) |= (1<<5))
+
+
+/* Clock Enable Macros for SYSCFG peripheral */
+#define SYSCFG_PCLK_EN()  (RCC->APB2ENR |= (1<<14))
+
+
+/* Clock Disable Macros for GPIOx peripherals */
+#define GPIOA_PCLK_DI()  (RCC->AHB1ENR &= ~(1<<0))
+#define GPIOB_PCLK_DI()  (RCC->AHB1ENR &= ~(1<<1))
+#define GPIOC_PCLK_DI()  (RCC->AHB1ENR &= ~(1<<2))
+#define GPIOD_PCLK_DI()  (RCC->AHB1ENR &= ~(1<<3))
+#define GPIOE_PCLK_DI()  (RCC->AHB1ENR &= ~(1<<4))
+#define GPIOF_PCLK_DI()  (RCC->AHB1ENR &= ~(1<<5))
+#define GPIOG_PCLK_DI()  (RCC->AHB1ENR &= ~(1<<6))
+#define GPIOH_PCLK_DI()  (RCC->AHB1ENR &= ~(1<<7))
+#define GPIOI_PCLK_DI()  (RCC->AHB1ENR &= ~(1<<8))
+
+
+/* Clock Disable Macros for I2Cx peripherals */
+#define I2C1_PCLK_DI()   (RCC->APB1ENR &= ~(1<<21))
+#define I2C2_PCLK_DI()   (RCC->APB1ENR &= ~(1<<22))
+#define I2C3_PCLK_DI()   (RCC->APB1ENR &= ~(1<<23))
+
+/* Clock Disable Macros for SPIx peripherals */
+#define SPI1_PCLK_DI()   (RCC->APB2ENR &= ~(1<<12))
+#define SPI2_PCLK_DI()   (RCC->APB1ENR &= ~(1<<15))
+#define SPI3_PCLK_DI()   (RCC->APB1ENR &= ~(1<<14))
+
+/* Clock Disable Macros for USARTx peripherals */
+#define USART1_PCLK_DI()  (RCC->APB2ENR) &= ~(1<<4))
+#define USART2_PCLK_DI()  (RCC->APB1ENR  &= ~(1<<17))
+#define USART3_PCLK_DI()  (RCC->APB1ENR  &= ~(1<<18))
+#define UART4_PCLK_DI()   (RCC->APB1ENR  &= ~(1<<19))
+#define UART5_PCLK_DI()   (RCC->APB1ENR  &= ~(1<<20))
+#define USART6_PCLK_DI()  (RCC->APB2ENR) &= ~(1<<5))
+
+
+/* Clock Disable Macros for SYSCFG peripheral */
+#define SYSCFG_PCLK_DI()  (RCC->APB2ENR) &= ~(1<<14))
+
+/* Macros to reset GPIOx peripherals */  // (RCC->AHB1ENR &= ~(1<<0))
+#define GPIOA_REG_RESET()   do{(RCC->AHB1RSTR |= (1<<0));  (RCC->AHB1RSTR &= ~(1<<0));}while(0)
+#define GPIOB_REG_RESET()   do{(RCC->AHB1RSTR |= (1<<1));  (RCC->AHB1RSTR &= ~(1<<1));}while(0)
+#define GPIOC_REG_RESET()   do{(RCC->AHB1RSTR |= (1<<2));  (RCC->AHB1RSTR &= ~(1<<2));}while(0)
+#define GPIOD_REG_RESET()   do{(RCC->AHB1RSTR |= (1<<3));  (RCC->AHB1RSTR &= ~(1<<3));}while(0)
+#define GPIOE_REG_RESET()   do{(RCC->AHB1RSTR |= (1<<4));  (RCC->AHB1RSTR &= ~(1<<4));}while(0)
+#define GPIOF_REG_RESET()   do{(RCC->AHB1RSTR |= (1<<5));  (RCC->AHB1RSTR &= ~(1<<5));}while(0)
+#define GPIOG_REG_RESET()   do{(RCC->AHB1RSTR |= (1<<6));  (RCC->AHB1RSTR &= ~(1<<6));}while(0)
+#define GPIOH_REG_RESET()   do{(RCC->AHB1RSTR |= (1<<7));  (RCC->AHB1RSTR &= ~(1<<7));}while(0)
+#define GPIOI_REG_RESET()   do{(RCC->AHB1RSTR |= (1<<8));  (RCC->AHB1RSTR &= ~(1<<8));}while(0)
+
+
+
+#define GPIO_BASEADDR_TO_CODE(x)        ((x == GPIOA)?0 :\
+		                                 (x == GPIOB)?1 :\
+		                                 (x == GPIOC)?2 :\
+                                         (x == GPIOD)?3 :\
+                                         (x == GPIOE)?4 :\
+                                         (x == GPIOF)?5 :\
+                                         (x == GPIOG)?6 :\
+                                         (x == GPIOH)?7 :0)
+
+
+
 
 #endif /* INC_STM32F411XX_H_ */
